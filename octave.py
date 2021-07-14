94e966c066c910c10073a29e5c3ffb4086563b97
liu mingzheng seiko.ac.jp raspberry pi <61229liu@seiko.ac.jp>
2021-07-14 11:10:21 +0900

    音は60段階っぽいです

--- octave.py
+++ octave.py
@@ -3,11 +3,11 @@ import random
 import numpy as np
 a, aa, b, c, cc, d, dd, e, f, ff, g, gg = np.geomspace(
     440, 880, 12, endpoint=False)
-OCTAVE = [c,cc, d,dd, e, f,ff, g,gg, a*2,aa*2,b*2]
+OCTAVE = [c, cc, d, dd, e, f, ff, g, gg, a*2, aa*2, b*2]
 # print(OCTAVE)
 
 
-def make_sound(distance: float,base=OCTAVE):
+def make_sound(distance: float, base=OCTAVE):
     if distance >= Telmin.DISTANCE_HIGH:
         return 0
     for i, j in zip(range(len(base))[::-1], np.linspace(0, Telmin.DISTANCE_HIGH, len(base)-1, endpoint=True)):
@@ -15,11 +15,14 @@ def make_sound(distance: float,base=OCTAVE):
             return int(round(base[i]))
 
 
+base = []
+for i in range(-2, 3):
+    base += map(lambda x: x*(2**i), OCTAVE)
+
+print(len(base))
 def main():
-    base=[]
-    for i in range(-2,3):
-        base+=map(lambda x:x*(2**i),OCTAVE)
-    sound_function=lambda x:make_sound(x,base)
+
+    def sound_function(x): return make_sound(x, base)
     termin = Telmin.Telmin()
     termin.main(sound_function)
 
