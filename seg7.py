de93358f64080126dabd8ad8c42389ca2ecf3e52
liu mingzheng seiko.ac.jp raspberry pi <61229liu@seiko.ac.jp>
2021-07-15 10:12:28 +0900

    7セグ使えました！！！！！！！

---
+++ seg7.py
@@ -0,0 +1,20 @@
+import datetime
+import time
+import tm1637
+tm = tm1637.TM1637(clk=21, dio=20)
+F=0b01110001
+U=0b00111110
+C=0b00111001
+K=0b01110110
+
+while 1:
+    now=datetime.datetime.now()
+    # print(now.hour,now.minute)
+    tm.numbers(now.hour,now.minute)
+    time.sleep(10)
+    
+    tm.write([F,U,C,K])
+    time.sleep(10)
+
+
+tm.scroll("FUCK", delay=250)
